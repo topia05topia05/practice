29435664b6dd44b54bd2ec1ddcb93aed57e83e93
topia05topia05 <topia05@ymail.ne.jp>
2025-04-24 12:49:58 +0900

    Create Book.java

---
+++ Book.java
@@ -0,0 +1,8 @@
+public class Book extends TangibleAsset{
+	private Stiing isbn;
+	public Book(string name,int price,String color,String isbn){
+		super(name,price,color);
+		this.isbn = isbn;
+	}
+	publc String getIsbn(){return thi.isbn;}
+}
